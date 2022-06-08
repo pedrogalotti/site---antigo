0288f2b248cf8f443a7524a532f6bed48d5bfaed
PedroLucas <93543449+pedrogalotti@users.noreply.github.com>
2022-06-08 10:52:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//const senha = document.querySelector('.senha');
+//const ocultar = document.querySelector('.ocultar');
+
+//ocultar.onclick = () => {
+//
+// if(senha.type == 'password'){
+//     senha.type = 'text'
+//      ocultar.src = 'ocultar.png'
+//  } else{
+//    senha.type = 'password'
+//    ocultar.src = 'mostrar.png'
+//  }
+//}
